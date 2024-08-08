8b57aac3945f13784b79ff276c714fd5e09b970c
Amrutgiri <apgoswami.info@gmail.com>
2024-08-08 14:36:55 +0530

    Show All users on Admin Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -23,8 +24,10 @@ export default function Dashboard() {
       </div>
       {/* Profile... */}
       {tab == "profile" && <DashProfile />}
-           {/* Post... */}
-           {tab=="posts" && <DashPosts/>}
+      {/* Post... */}
+      {tab=="posts" && <DashPosts/>}
+      {/* Users... */}
+      {tab=="users" && <DashUsers/>}
     </div>
   );
 }
