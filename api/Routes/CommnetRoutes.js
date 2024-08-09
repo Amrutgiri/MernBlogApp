4ac1e9bf8f9105bad18fa0d555c3c593d9e59c08
Amrutgiri <apgoswami.info@gmail.com>
2024-08-09 17:57:51 +0530

    Show Comment of the post

--- api/Routes/CommnetRoutes.js
+++ api/Routes/CommnetRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createComment } from '../Controllers/CommentController.js';
+import { createComment, getPostComments } from '../Controllers/CommentController.js';
 import {verifyToken} from '../Utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,createComment);
-
+router.get('/getPostComments/:postId',getPostComments);
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,12 +1,14 @@
 import { Alert, Button, TextInput } from 'flowbite-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
 import { Link } from 'react-router-dom'
+import Comment from './Comment'
 
 export default function CommentSection({postId}) {
     const {currentUser} = useSelector((state) => state.user)
     const [comment,setComment]=useState('');
     const [commentError,setCommentError]=useState(null);
+    const [comments,setComments]=useState([]);
     const handleSubmit = async(e) => {
         e.preventDefault();
         if(comment.length>200)
@@ -25,12 +27,27 @@ export default function CommentSection({postId}) {
             if(res.ok){
                 setComment('');
                 setCommentError(null);
+                setComments([data,...comments]);
             }
         } catch (error) {
             setCommentError(error.message);
         }
     }
 
+    useEffect(() => {
+        const getPostComments = async() => {
+            try {
+                const res =await fetch(`/api/comment/getPostComments/${postId}`);
+                if(res.ok){
+                    const data=await res.json();
+                    setComments(data);
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        getPostComments();
+    },[postId])
 
   return (
     <div className='w-full max-w-2xl p-3 mx-auto'>
@@ -66,13 +83,33 @@ export default function CommentSection({postId}) {
                         Submit
                     </Button>
                 </div>
-                {commentError && (   
+                {
+                commentError && (   
                 <Alert color={'failure'} className='mt-5'>
                     {commentError}
-                </Alert>
-                )}
+                </Alert>)
+                }
             </form>
         )}
+
+        {comments.length===0  ? (
+            <p className='my-5 text-sm'>No Comments yet !</p>
+        ):(
+            <>
+            <div className='flex items-center gap-1 my-5 text-sm'>
+                <p>Comments</p>
+                <div className='px-2 py-1 border border-gray-400 rounded-sm'>
+                    <p>{comments.length}</p>
+                </div>
+            </div>
+            {
+                comments.map(comment => ( 
+                    <Comment key={comment._id} comment={comment} />
+                ))
+            }
+            </>
+        )
+        }
     </div>
   )
 }
