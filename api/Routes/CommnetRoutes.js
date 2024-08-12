3d804a822aabc0ecf68c3fca0cd4f4436793398f
Amrutgiri <apgoswami.info@gmail.com>
2024-08-12 16:21:30 +0530

    Add Comments Section on Dashboard

--- api/Routes/CommnetRoutes.js
+++ api/Routes/CommnetRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, deleteComment, editComment, getPostComments, likeComment } from '../Controllers/CommentController.js';
+import { createComment, deleteComment, editComment, getcomments, getPostComments, likeComment } from '../Controllers/CommentController.js';
 import {verifyToken} from '../Utils/verifyUser.js';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get('/getPostComments/:postId',getPostComments);
 router.put('/likecomment/:commentId',verifyToken,likeComment);
 router.put('/editcomment/:commentId',verifyToken,editComment);
 router.delete('/deletecomment/:commentId',verifyToken,deleteComment);
+router.get('/getcomments',verifyToken,getcomments);
 
 export default router;
