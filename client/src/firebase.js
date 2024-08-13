baef76ef0a892c00415e2b8529369f3765e08f8b
Amrutgiri <apgoswami.info@gmail.com>
2024-08-13 15:02:38 +0530

    firebase change

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,14 +4,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_FIREBASE_KEY,
+//     authDomain: "mern-blog-c1c24.firebaseapp.com",
+//     projectId: "mern-blog-c1c24",
+//     storageBucket: "mern-blog-c1c24.appspot.com",
+//     messagingSenderId: "445369683164",
+//     appId: "1:445369683164:web:b07c4c25a2f32195d63819"
+// };
+
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_KEY,
     authDomain: "mern-blog-c1c24.firebaseapp.com",
+    databaseURL: "mern-blog-c1c24.appspot.com",
     projectId: "mern-blog-c1c24",
-    storageBucket: "mern-blog-c1c24.appspot.com",
-    messagingSenderId: "445369683164",
     appId: "1:445369683164:web:b07c4c25a2f32195d63819"
-};
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
