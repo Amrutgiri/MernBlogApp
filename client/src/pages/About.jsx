9566165d7f37701aee61a7461b67699194c3d0eb
Amrutgiri <apgoswami.info@gmail.com>
2024-08-13 13:05:02 +0530

    Complete About and Project Pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className='max-w-2xl p-3 mx-auto text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center font my-7'>About EWW Blog</h1>
+          <div className='flex flex-col gap-6 text-gray-500 text-md'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum asperiores quo quia aliquid voluptates, officia eum repellendus laudantium vel, atque accusantium omnis aut veniam molestiae nam dolorem libero quod illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi obcaecati a consequuntur, molestiae quidem officiis ab beatae, aperiam hic expedita ipsa animi sint odit aliquam, ea magni explicabo placeat aut. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, rem? Quia ullam modi minus doloribus officia beatae, quam perspiciatis quasi laborum, deleniti numquam et inventore, dignissimos dolorem tempore? Commodi, inventore!</p>
+
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur omnis laudantium, odit ullam porro corporis recusandae eveniet. Itaque, sit eaque debitis accusamus quos laborum harum corrupti aliquam suscipit, similique dicta! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, ex praesentium culpa laboriosam asperiores quas, officiis, nesciunt ipsa dolorem ratione consectetur tempore incidunt? Cupiditate facilis ad dolor commodi, vel quasi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ea nam saepe eius, dolorem asperiores neque est optio hic porro excepturi distinctio fugiat iste at. Velit dicta delectus aut maiores?
+            </p>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit dolorem reprehenderit nihil ullam aspernatur distinctio quo minima facilis ea repudiandae unde, accusantium id, reiciendis vero ex. Harum voluptatibus dolore enim! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non minima cum delectus totam veritatis a facilis aut, eos hic. Tempore nulla nihil, fugit unde facere sit et nobis eligendi vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut voluptatem molestias amet! Consequuntur praesentium quibusdam molestiae nam. Ut dignissimos incidunt eveniet rem itaque nemo nostrum sit alias, minima similique?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import CallToAction from '../components/CallToAction'
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='flex flex-col items-center justify-center max-w-2xl min-h-screen gap-6 p-4 mx-auto'>
+      <h1 className='mb-4 text-3xl font-semibold'>Projects</h1>
+      <p className='text-gray-500 text-md'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam hic quis repudiandae quisquam aut odit vel cupiditate, beatae, quo fugiat deserunt. Optio mollitia ullam tempora eaque quam non aliquid ipsum!</p>
+      <CallToAction />
+    </div>
   )
 }
